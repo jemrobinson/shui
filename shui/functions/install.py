c99cf24ea942a6d688c06a3daf762707bb8d4ada
James Robinson <james.em.robinson@gmail.com>
2021-03-16 18:08:23 +0000

    Catch not-a-tarball errors when extracting

--- shui/functions/install.py
+++ shui/functions/install.py
@@ -5,14 +5,15 @@ import tarfile
 def extract_tarball(tarball, install_dir):
     """Extract tarball to a local path"""
     if not tarball.path.is_file():
-        raise ValueError(
-            f"Could not find a valid tarball at <info>{tarball.path}</info>!"
-        )
-    with tarfile.open(tarball.path, "r:gz") as f_tarball:
-        extraction_dir = [
-            obj.name
-            for obj in f_tarball.getmembers()
-            if obj.isdir() and "/" not in obj.name
-        ][0]
-        f_tarball.extractall(install_dir)
+        raise IOError(f"<info>{tarball.path}</info> is not a file!")
+    try:
+        with tarfile.open(tarball.path, "r:gz") as f_tarball:
+            extraction_dir = [
+                obj.name
+                for obj in f_tarball.getmembers()
+                if obj.isdir() and "/" not in obj.name
+            ][0]
+            f_tarball.extractall(install_dir)
+    except tarfile.ReadError as exc:
+        raise IOError(f"<info>{tarball.path}</info> is not a valid tarball!") from exc
     return install_dir / extraction_dir
