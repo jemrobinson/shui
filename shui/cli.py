f7d458dfebf49210761e285c70eb07d13e17614d
James Robinson <james.em.robinson@gmail.com>
2022-08-18 17:05:27 +0100

    :bookmark: Bump to version 0.6.0

--- shui/cli.py
+++ shui/cli.py
@@ -1,8 +1,9 @@
 """Command line entrypoint for shui application"""
 from cleo import Application
 from shui.commands import InstallCommand, VersionsCommand
+from shui import __version__
 
-application = Application("shui", "0.6.0", complete=True)
+application = Application("shui", __version__, complete=True)
 application.add(InstallCommand())
 application.add(VersionsCommand())
 
