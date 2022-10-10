4ff15c8691077bddd37042b2d5482f8087575f8f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 06:03:39 +0000

    Adding tarfile member sanitization to extractall()

--- shui/functions/install.py
+++ shui/functions/install.py
@@ -15,7 +15,29 @@ def extract_tarball(tarball: FileInfo, install_dir: pathlib.Path) -> pathlib.Pat
                 for obj in f_tarball.getmembers()
                 if obj.isdir() and "/" not in obj.name
             ][0]
-            f_tarball.extractall(install_dir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f_tarball, install_dir)
     except tarfile.ReadError as exc:
         raise IOError(f"<info>{tarball.path}</info> is not a valid tarball!") from exc
     return install_dir / extraction_dir
