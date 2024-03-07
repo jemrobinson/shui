d8c0824db401c144813ba157ce671a785ec345e4
James Robinson <james.em.robinson@gmail.com>
2024-03-07 16:29:10 +0000

    :coffin: Drop Python 3.7 support

--- shui/__init__.py
+++ shui/__init__.py
@@ -1,8 +1,4 @@
 """Base include for shui (Spark-Hadoop Unix Installer)"""
-try:
-    from importlib import metadata
-except ImportError:
-    # This is needed to support Python 3.7
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __version__ = metadata.version("shui")
