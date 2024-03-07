7693506f86ecc82a1e7df4819ed615892d88f0fb
James Robinson <james.em.robinson@gmail.com>
2024-03-07 16:13:30 +0000

    :bug: Allow importlib to work in Python 3.7

--- shui/__init__.py
+++ shui/__init__.py
@@ -1,4 +1,8 @@
 """Base include for shui (Spark-Hadoop Unix Installer)"""
-from importlib import metadata
+try:
+    from importlib import metadata
+except ImportError:
+    # This is needed to support Python 3.7
+    import importlib_metadata as metadata
 
 __version__ = metadata.version("shui")
